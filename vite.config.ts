50599ae37d9f0dcf436edb9e11083c38b5b6398d
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-05-27 21:14:14 +0530

    #

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  optimizeDeps: {
+    exclude: ['blip-ds/loader']
+  },
   plugins: [react()],
   server: {
     port: 3000,
