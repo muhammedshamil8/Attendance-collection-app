af1f1d51f75888cc491570672b72fd7390ee97c9
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-05-27 21:17:38 +0530

    Updated: theme button

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -8,18 +8,35 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { useTheme } from "@/components/theme-provider"
+import { useRef } from "react"
 
-export function ModeToggle() {
+export function ModeToggle({ icon = true, text = "Toggle theme" }) {
   const { setTheme } = useTheme()
+  const iconState = useRef(icon).current
+
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 " />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-white" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
+        {iconState ? (
+          <Button variant="outline" size="icon">
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 " />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-white" />
+            <span className="sr-only">Toggle theme</span>
+          </Button>
+        ) : (
+          <Button variant="outline" className="text-black dark:text-white flex items-center justify-center gap-2 font-medium w-full border border-gray-300">
+            <div className="flex">
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 " />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-white" />
+            </div>
+            <span>
+            {text}
+            </span>
+          </Button>
+        )
+        }
+
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
