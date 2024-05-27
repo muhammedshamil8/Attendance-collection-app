786d93d5d64292b9a448f7e8530bfda71ae47d65
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-05-27 21:19:16 +0530

    implement badge comp

---
+++ src/components/ActiveBadge.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ActiveBadge() {
+    return (
+        <div className='absolute -top-1 -right-1'>
+            <span className="relative flex h-3 w-3">
+                <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+                <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+            </span>
+        </div>
+    )
+}
+
+export default ActiveBadge
