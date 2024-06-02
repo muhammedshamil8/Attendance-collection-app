098c9155169a3ef9b94464c5fd42e6d355de4fe6
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-06-02 17:47:34 +0530

    Feat: icon , sw .. setuped

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,52 @@ import { ThemeProvider } from "@/components/theme-provider"
 import router from '@/routes'
 import { Toaster } from '@/components/ui/toaster'
 import '@/styles/global.css'
+import { AnimatePresence } from "framer-motion";
+
+const updateMetaThemeColor = (theme: string) => {
+  const metaThemeColor = document.querySelector("meta[name=theme-color]");
+  const metaBackgroundColor = document.querySelector("meta[name=background-color]");
+
+  if (metaThemeColor && metaBackgroundColor) {
+    switch (theme) {
+      case 'dark':
+        metaThemeColor.setAttribute("content", "#e2e8f0");
+        metaBackgroundColor.setAttribute("content", "#0f172a");
+        break;
+      case 'light':
+        metaThemeColor.setAttribute("content", "#059669");
+        metaBackgroundColor.setAttribute("content", "#e2e8f0");
+        break;
+      default:
+        metaThemeColor.setAttribute("content", "#000000");
+        metaBackgroundColor.setAttribute("content", "#ffffff");
+    }
+  }
+};
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-    <RouterProvider router={router} />
+      <AnimatePresence  mode='wait'>
+        <RouterProvider router={router} />
+      </AnimatePresence>
     </ThemeProvider>
     <Toaster />
   </React.StrictMode>,
 )
+
+// Retrieve the stored theme from localStorage
+const storedTheme = localStorage.getItem('vite-ui-theme') || 'dark';
+
+// Initially update the theme color based on the stored theme
+updateMetaThemeColor(storedTheme);
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/src/service-worker.ts').then(registration => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    }).catch(error => {
+      console.error('Service Worker registration failed:', error);
+    });
+  });
+}
