1aebd70fdb969efa09ed4fb5c8f932f6783a4b81
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-06-02 17:49:16 +0530

    Updated: content

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,92 +1,79 @@
 import React, { useState } from 'react';
 import { useAutoAnimate } from '@formkit/auto-animate/react'
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
-import { FaLinkedinIn , FaInstagram ,  } from "react-icons/fa";
+import { FaLinkedinIn, FaInstagram, } from "react-icons/fa";
 import { IoMailOutline } from "react-icons/io5";
 
-interface Story {
-    title: string;
-    showStory: boolean;
-    story: string;
+interface FAQ {
+    question: string;
+    isOpen: boolean;
+    answer: string;
 }
 
 const About: React.FC = () => {
     const [parent] = useAutoAnimate();
-    const [Accordion, setAccordion] = useState<Story[]>([
-        { title: 'For What this App developed?', showStory: false, story: "This app is developed for collecting students data much faster for an  each event conducting EMEA college" },
-        { title: 'How to use this App?', showStory: false, story: "This app is very simple to use. Just click on the 'Add, Student' button and fill the form with the student's details. Then click on the 'Submit' button to save the student's details."},
-        { title: 'How to delete a student?', showStory: false, story: "To delete a student, click on the 'Delete' button on the student's card."},
-        { title: 'Can i add any student?', showStory: false, story: "No, you can't add any student. You can only add students who are studying in EMEA college."},
-        { title: 'Who can use this App?', showStory: false, story: "This app is only for the EMEA college staff. They can use this app to collect students' data for each event."},
-        { title: 'How to contact the developer?', showStory: false, story: "You can contact the developer by clicking on the '...Me...' link at the bottom of the page." },
+    const [faqs, setFaqs] = useState<FAQ[]>([
+        { question: 'What is the purpose of this app?', isOpen: false, answer: "MARK !T is designed to streamline the process of collecting student data during events organized by EMEA College clubs and departments. It allows volunteers to quickly add students to event lists by entering their admission numbers, reducing the time and effort required for data collection." },
+        { question: 'How do I use this app?', isOpen: false, answer: "Using MARK !T is simple. Just click on the 'Add Student' button and fill in the student's details. Then, click 'Submit' to save the student's information." },
+        { question: 'How can I delete a student?', isOpen: false, answer: "To delete a student, simply click on the 'Delete' button on the student's card." },
+        { question: 'Can I add any student?', isOpen: false, answer: "No, you can only add students who are currently studying at EMEA College." },
+        { question: 'Who can use this app?', isOpen: false, answer: "This app is intended for use by EMEA College staff, specifically for collecting student data during events." },
+        { question: 'How can I contact the developer?', isOpen: false, answer: "You can reach out to the developer by clicking on the '...Me...' link at the bottom of the page." },
         {
-            title: 'Who developed this App?',
-            showStory: false,
-            story: "This app is developed by Zamil. He is a full-stack developer with 3 years of experience in web development. He is passionate about learning new technologies and building web applications."
+            question: 'Who developed and designed this app?',
+            isOpen: false,
+            answer: "MARK !T was developed by Muhammed Shamil, a BSc Computer Science 2nd year student at EMEA College, with design by Dayyan Ali, also a student at EMEA College. The app was conceptualized by Hasil, the IEDC CCO, and implemented with the support of the IEDC technical team."
         },
         {
-            title: 'Who desined this App?',
-            showStory: false,
-            story: "This app is designed by dayyan. He is a UI/UX designer with 2 years of experience in designing web applications. He is passionate about creating user-friendly interfaces and designing beautiful websites."
-        },
-        {
-            title: 'whose idea is this App?',
-            showStory: false,
-            story: "This app is an idea of the IEDC CCO Hasil. He is a creative thinker with 5 years of experience in entrepreneurship. He is passionate about creating innovative solutions and building successful businesses."
+            question: 'How can new club core teams connect with us?',
+            isOpen: false,
+            answer: "New club core teams can connect with us through the contact section of the app. They can request an account, which will be verified by the admin. Once verified, they will receive an email with login credentials. Using their account, they can create, edit, and delete events, add students to events, scan IDs, export data, and more. If a student does not exist in the system, they can create a new student entry."
         }
-
-
     ]);
+    
 
     return (
         <div className="text-black dark:text-white p-8 pb-2 m-4 flex flex-col justify-between min-h-screen mb-20">
             <div>
-            <h1 className="text-4xl font-bold mb-4">About This App</h1>
-            <p className="text-lg mb-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
-                justo et nunc tincidunt, in tristique tellus consectetur. Nullam
-                aliquet, mauris ac lacinia tincidunt, est ex aliquet nunc, id
-                ullamcorper mauris nisl ac libero.
-            </p>
-            <p className="text-lg mb-4">
-                Fusce auctor, nunc id aliquet fringilla, nisl nunc ultrices mi, eu
-                tincidunt mauris ex nec nunc. Sed id enim auctor, sollicitudin leo
-                vitae, pharetra nunc. Sed at justo id nunc lacinia viverra.
-            </p>
-            <p className="text-lg mb-4">
-                Donec euismod, nunc et consequat tincidunt, nunc nunc aliquam nunc, id
-                consectetur felis nunc vel nunc. Sed euismod, nunc et consequat
-                tincidunt, nunc nunc aliquam nunc, id consectetur felis nunc vel nunc.
-            </p>
-            <p className="text-lg mb-4">
-                Duis vel semper nunc. Nullam euismod, nunc et consequat tincidunt,
-                nunc nunc aliquam nunc, id consectetur felis nunc vel nunc.
-            </p>
-            <div className='mt-10'>
-                <h3 className='text-left dark:text-white font-semibold text-lg'>FAQs</h3>
-                {Accordion.map((item, index) => (
-                    <div key={index} className="">
-                        <h2 className='p-2 font-semibold cursor-pointer hover:underline transition-all ease-in-out flex items-center justify-between border-b border-gray-300 dark:border-b dark:border-gray-700 text-lg'
-                            onClick={() => setAccordion(Accordion.map((s, i) => i === index ? { ...s, showStory: !s.showStory } : s))}>
-                            {item.title}?
-                            {item.showStory ? <IoIosArrowUp /> : <IoIosArrowDown />}
-                        </h2>
-                        <div ref={parent}>{item.showStory ? <div>{item.story}</div> : null}</div>
-                    </div>
-                ))}
-            </div>
+                <h1 className="text-4xl font-bold mb-4">About MARK !T</h1>
+                <p className="text-lg mb-4">
+                    MARK !T is a solution developed for EMEA College to streamline the process of collecting student data during events organized by clubs and other departments. Previously, volunteers had to manually write down details such as admission number, name, roll number, department, and whether they are undergraduate or postgraduate students. This manual process was not only time-consuming but also prone to errors, especially during data sorting and entry into the attendance software.
+                </p>
+                <p className="text-lg mb-4">
+                    With MARK !T, volunteers can easily add students to the event list by entering their admission numbers. This significantly reduces the time and effort required for data collection. Additionally, multiple volunteers can simultaneously access a live table displaying student data, making coordination more efficient. The app also features a scan ID card functionality, allowing for even faster entry of admission numbers.
+                </p>
+                <p className="text-lg mb-4">
+                    One of the key features of MARK !T is its ability to export student data to PDF or Excel format, sorted by department and year. This feature ensures that the data is organized and easily accessible for further processing. Overall, MARK !T provides a user-friendly and efficient solution for managing student data during college events, enhancing the experience for both volunteers and the core team of the clubs.
+                </p>
+                <p className="text-lg mb-4">
+                    In addition to its event management capabilities, MARK !T also includes an admin panel for managing students and users. This panel allows administrators to control access, monitor activity, and perform various CRUD operations to maintain the system.
+                </p>
+
+                <div className='mt-10'>
+                    <h3 className='text-left dark:text-white font-semibold text-lg'>FAQs</h3>
+                    {faqs.map((item, index) => (
+                        <div key={index} className="">
+                            <h2 className='p-2 font-semibold cursor-pointer hover:underline transition-all ease-in-out flex items-center justify-between border-b border-gray-300 dark:border-b dark:border-gray-700 text-lg'
+                                onClick={() => setFaqs(faqs.map((s, i) => i === index ? { ...s, isOpen: !s.isOpen } : s))}>
+                                {item.question}?
+                                {item.isOpen ? <IoIosArrowUp /> : <IoIosArrowDown />}
+                            </h2>
+                            <div ref={parent}>{item.isOpen ? <div className='text-md py-2'>{item.answer}</div> : null}</div>
+                        </div>
+                    ))}
+                </div>
             </div>
 
-            
+
 
             <p className='flex gap-2 mx-auto mt-10'>
-                <FaLinkedinIn className='text-xl m-2 text-gray-500 cursor-pointer hover:text-gray-700 transition-all ease-in-out' onClick={() => window.open('https://www.linkedin.com/in/zamil-ahamed-1b1b3b1b1/')} />
+                <FaLinkedinIn className='text-xl m-2 text-gray-500 cursor-pointer hover:text-gray-700 transition-all ease-in-out' onClick={() => window.open('https://www.linkedin.com/company/iedcemea')} />
                 <FaInstagram className='text-xl m-2 text-gray-600 cursor-pointer hover:text-gray-700 transition-all ease-in-out' onClick={() => window.open('https://www.instagram.com/zamilahamed/')} />
                 <IoMailOutline className='text-xl m-2 text-gray-600 cursor-pointer hover:text-gray-700 transition-all ease-in-out' onClick={() => window.open('mailto:emeaiedc@gmail.com')} />
             </p>
 
             <p className='text-center mt-10'>
-                crafted with <span className='text-red-600'>❤</span> by <a href="https://zamil.me" className='hover:underline'>...Me...</a>
+                crafted with <span className='text-red-600'>❤</span> by <a href="https://zamil.me" className='hover:underline'> ...Me... </a>
             </p>
         </div>
     );
